aeb5da2ff9d7198165a880f3b3944c36b2687478
Muhammad Saeed Arabi <muhammadsaeed_express@yahoo.com>
2018-06-09 11:14:14 +0300

    add plans app

---
+++ easy_internship_api/plans/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Requirement(models.Model):
+    pass
+
+
+class Blueprint(models.Model):
+    pass
+
+
+# ? TODO: Use state (e.g. django-fsm ?) to keep track of changes in following models
+
+# ? TODO: more generic name
+class Rotation(models.Model):
+    # TODO
+    # ? state = FSMField
+    # ? pending_update = JSONField
+    pass
+
+
+# ? TODO: more generic name
+class Freeze(models.Model):
+    # TODO
+    # ? state = FSMField
+    # ? pending_update = JSONField
+    pass
+
+
+class Leave(models.Model):
+    # TODO
+    # ? state = FSMField
+    # ? pending_update = JSONField
+    pass
